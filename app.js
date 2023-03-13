c40df78dc4f5288bfd7b5ae130a598096747dd44
YING LIANG <y.liang.168@westcliff.edu>
2023-03-13 00:12:38 -0500

    commit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const { MongoClient } = require('mongodb')
+
+//create instance of mongoclient for mongodb
+const client = new MongoClient('mongodb://127.0.0.1:27017/nodemongo')
+
 var stats =[
     {
         'city': 'San Juan', 
@@ -42,3 +47,84 @@ var stats =[
         'age': '35'
     }
 ]
+
+
+
+
+
+//connect to database
+
+client.connect()
+.then(() => {
+    // console.log('Connected to MongoDB');
+    // //close connection
+    // client.close();   
+    // console.log("database created");
+
+    dbo=client.db('statsdb');
+    /*dbo.createCollection('uscensus').then(() => {
+        console.log("collection created");
+        client.close();
+    });*/
+
+    /*dbo.collection('uscensus').insertMany(stats).then((res) => {
+        console.log(console.log('Number of documents inserted: ' + res.insertedCount));
+        client.close();
+    });*/
+
+    var stats2=[
+        { 'city': 'SPacoima', 
+        'zip': '91331', 
+        'state': 'CA', 
+        'income': '60360',
+        'age': '33'
+    },
+
+    {'city': 'Ketchikan', 
+    'zip': '99950', 
+    'state': 'AK', 
+    'income': '00000',
+    'age': '00'
+
+    }
+]
+
+   /* dbo.collection('uscensus').insertMany(stats2).then((res) => {
+        console.log(console.log('Number of documents inserted: ' + res.insertedCount));
+    });*/
+    // var query={city:"Corona"}
+    // dbo.collection('uscensus').findOne(query).then((res) => {
+    //     console.log(res.zip);
+     // client.close();
+    // });
+
+    // var query={state:"CA"};
+    // var CAcity=dbo.collection('uscensus').find(query).toArray().then((res) => {
+
+    //     for(var i=0;i<res.length;i++){
+    //     console.log(`The income of ${res[i].city} is ${res[i].income}`);
+    //     }
+    // client.close();
+  
+    // })
+    // var query={city:"Alaska"};
+    // var newValue={$set:{zip:38910, income:46}};
+    // dbo.collection('uscensus').updateOne(query,newValue).then(() => {
+    //     console.log("1 document updated");
+    //     client.close();
+    // })
+
+    var mysort={state:1};
+    dbo.collection('uscensus').find().sort(mysort).toArray().then((item) => {
+        console.log('Successfully sorted by state');
+        console.log(item);
+        client.close();
+    });
+
+ 
+})
+.catch(error=>console.log('Failed to connect to MongoDB', error))
+
+
+
+
